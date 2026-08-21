ab7c0a44885b10db1646ccf60f5c3fb59e1f62aa
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-08-21 14:00:06 +0530

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -27,4 +27,86 @@ account.balance = -50000
 print(account.balance)
 Output:
 -50000
-  
+
+With encapsulation:
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+    def get_balance(self):
+        return self.__balance
+account = BankAccount(10000)
+account.deposit(5000)
+account.withdraw(2000)
+print(account.get_balance())
+The user doesn't directly manipulate the internal balance.
+
+
+1c) Access Control in Python
+Python doesn't have strict access modifiers like Java:
+public
+private
+protected
+Instead, Python uses naming conventions.
+There are mainly three levels:
+Syntax	Meaning
+name	Public
+_name	Protected convention
+__name	Private/name mangling
+
+1c1) Public Members
+A public member can be accessed from anywhere.
+Example:
+class Employee:
+    def __init__(self):
+        self.name = "Chandana"
+        self.salary = 50000
+employee = Employee()
+print(employee.name)
+print(employee.salary)
+Output:
+Chandana
+50000
+You can also modify them:
+employee.name = "Rahul"
+employee.salary = 60000
+So:
+self.name
+self.salary
+are public attributes.
+
+1c2) Protected Members — _name
+Python uses a single underscore to indicate that a member is intended for internal or subclass use.
+Example:
+class Employee:
+    def __init__(self):
+        self.name = "Chandana"
+        self._salary = 50000
+employee = Employee()
+print(employee.name)
+print(employee._salary)
+Python does not actually prevent access.
+The underscore is a convention.
+It tells other developers:
+"This is intended for internal use. Please don't access it directly unless you know what you're doing."
+    Protected with inheritance
+This is where _name becomes particularly useful.
+class Employee:
+    def __init__(self):
+        self._salary = 50000
+class Manager(Employee):
+    def show_salary(self):
+        print(self._salary)
+manager = Manager()
+manager.show_salary()
+Output:
+50000
+The child class can access _salary.
+So conventionally:
+name       → Public
+_name      → Internal / subclass use
